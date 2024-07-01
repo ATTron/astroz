68cc865bffa9a391a98e945a8cd551e118faa760
Anthony Templeton <anthonyt@duck.com>
2024-07-01 01:50:15 -0400

    more vita49 updates

--- src/vita49.zig
+++ src/vita49.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const debug = std.debug;
 
 pub const Packet_Type = enum(u4) {
-    signal_w_stream_id = 0,
-    signal_wo_stream_id = 1,
+    signal_wo_stream_id = 0,
+    signal_w_stream_id = 1,
     ext_data_wo_steam_id = 2,
     ext_data_w_steam_id = 3,
     ctx_packet = 4,
@@ -12,12 +12,27 @@ pub const Packet_Type = enum(u4) {
     ext_cmd_packet = 7,
 };
 
+pub const TSF = enum(u2) {
+    none = 0,
+    sample_count = 1,
+    real_time = 2,
+    free_running_count = 3,
+};
+
+pub const TSI = enum(u2) {
+    none = 0,
+    utc = 1,
+    gps = 2,
+    other = 3,
+};
+
 pub const Header = packed struct {
     packet_type: Packet_Type,
-    c: bool,
-    indicator: u3,
-    tsi: u2,
-    tsf: u2,
+    class_id: bool,
+    trailer: bool,
+    metadata: bool,
+    tsi: TSI,
+    tsf: TSF,
     packet_count: u4,
     packet_size: u16,
 
@@ -25,18 +40,26 @@ pub const Header = packed struct {
 
     pub fn new(stream: [4]u8) !Self {
         const little_endian_stream = std.mem.readInt(u32, &stream, .little);
-
-        std.debug.print("\nbig endian = {x}\nlittle endian = {x}\n", .{ stream, little_endian_stream });
-
         const packet_as_uint = @as(u4, @truncate((little_endian_stream >> 28) & 0xF));
-        const c = ((little_endian_stream >> 27) & 1) == 1;
-        const indicator = @as(u3, @truncate((little_endian_stream >> 24) & 0x7));
         const tsi = @as(u2, @truncate((little_endian_stream >> 22) & 0x3));
-        const tsf = @as(u2, @truncate((little_endian_stream >> 20) & 0x3));
-        const packet_count = @as(u4, @truncate((little_endian_stream >> 14) & 0xF));
-        const packet_size = @as(u16, @truncate(little_endian_stream & 0xFFFF));
+        const tsf = @as(u2, @truncate((little_endian_stream >> 24) & 0x3));
 
-        return .{ .packet_type = @enumFromInt(packet_as_uint), .c = c, .indicator = indicator, .tsi = tsi, .tsf = tsf, .packet_count = packet_count, .packet_size = packet_size };
+        std.debug.print("\nshowing entire stream: {x}\n", .{little_endian_stream});
+        std.debug.print("\nshowing tsi area probably: {x}\n", .{little_endian_stream >> 22});
+
+        return .{ .packet_type = @enumFromInt(packet_as_uint), .class_id = ((little_endian_stream >> 27) & 1) == 1, .trailer = ((little_endian_stream >> 24) & 1) == 1, .metadata = ((little_endian_stream >> 26) & 1) == 1, .tsi = @enumFromInt(tsi), .tsf = @enumFromInt(tsf), .packet_count = @as(u4, @truncate((little_endian_stream >> 14) & 0xF)), .packet_size = @as(u16, @truncate(little_endian_stream & 0xFFFF)) };
+    }
+
+    pub fn output(self: Self) void {
+        std.debug.print("Vita49 Packet Header:\n", .{});
+        std.debug.print("packet type: {}\n", .{self.packet_type});
+        std.debug.print("class ID: {}\n", .{self.class_id});
+        std.debug.print("Trailer Present: {}\n", .{self.trailer});
+        std.debug.print("Metadata Present: {}\n", .{self.metadata});
+        std.debug.print("tsi: {}\n", .{self.tsi});
+        std.debug.print("tsf: {}\n", .{self.tsf});
+        std.debug.print("packet_count: {}\n", .{self.packet_count});
+        std.debug.print("packet_size: {}\n", .{self.packet_size});
     }
 };
 
@@ -70,10 +93,9 @@ pub const Vita49 = struct {
 
 pub const Vita49_Parser = struct { stream: []const u8, packets: []const Vita49 };
 
-test "Test New Vita49 Header" {
-    const test_packet = [_]u8{ 0x1C, 0x00, 0x30, 0x18 };
+test "Test Vita49 Header" {
+    const test_packet = [_]u8{ 0x1C, 0x00, 0xC0, 0x18 };
 
     const header = try Header.new(test_packet);
-
-    std.debug.print("\nHEADER IS: {any}\n", .{header});
+    header.output();
 }
