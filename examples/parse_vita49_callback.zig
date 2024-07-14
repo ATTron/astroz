7fe66d174ab0d52aa3b65e7c58df513eead8c37f
Jora Troosh <tensorush@gmail.com>
2024-07-14 19:21:25 +0300

    feat: add example suite (#18)

---
+++ examples/parse_vita49_callback.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const astroz = @import("astroz");
+const Vita49 = astroz.vita49.Vita49;
+const Parser = astroz.parsers.Parser;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const P = Parser(Vita49);
+    const ip = "127.0.0.1".*;
+    const port: u16 = 65432;
+    var parser = try P.init(&ip, port, 1024, allocator);
+    defer parser.deinit();
+    _ = try parser.start(callback);
+}
+
+fn callback(packet: Vita49) void {
+    std.debug.print("Packet received: {any}", .{packet});
+}
