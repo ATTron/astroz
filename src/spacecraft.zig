8a4070a4145a07b9449098eebac77b44beeed686
Anthony Templeton <twantempleton11@gmail.com>
2024-07-14 23:38:36 -0400

    add dramatic attitude changes test data

--- src/spacecraft.zig
+++ src/spacecraft.zig
@@ -854,3 +854,66 @@ test "orientation determination testing" {
         // });
     }
 }
+
+test "orientation determination with dramatic changes" {
+    const raw_tle =
+        \\1 55909U 23035B   24187.51050877  .00023579  00000+0  16099-2 0  9998
+        \\2 55909  43.9978 311.8012 0011446 278.6226  81.3336 15.05761711 71371
+    ;
+    var test_tle = try TLE.parse(raw_tle, std.testing.allocator);
+    defer test_tle.deinit();
+    var spacecraft = Spacecraft.init("dummy_sc", test_tle, 300.000, SatelliteSize.Cube, constants.earth, std.testing.allocator);
+    defer spacecraft.deinit();
+
+    // Initial angular velocity
+    spacecraft.angular_velocity = .{ 0.0, 0.0, 0.0 };
+
+    // const file = try std.fs.cwd().createFile("test/dramatic_attitude_data.csv", .{});
+    // defer file.close();
+    // const writer = file.writer();
+
+    // try writer.writeAll("Time,qw,qx,qy,qz,wx,wy,wz,x,y,z\n");
+
+    const dt = 120.0; // 2 mins time step
+    const simulation_time = 3 * 24 * 60 * 60.0; // 3 days in seconds
+    const orbital_period = 90 * 60.0; // 90 minutes orbital period
+    var t: f64 = 0;
+
+    while (t < simulation_time) : (t += dt) {
+        // Simulate a dramatic torque effect
+        const torque_x = 0.001 * @sin(2 * std.math.pi * t / (orbital_period * 2));
+        const torque_y = 0.0005 * @cos(2 * std.math.pi * t / (orbital_period * 3));
+        const torque_z = 0.0002 * @sin(2 * std.math.pi * t / orbital_period);
+
+        // Update angular velocity based on torque (simplified)
+        spacecraft.angular_velocity[0] += torque_x * dt;
+        spacecraft.angular_velocity[1] += torque_y * dt;
+        spacecraft.angular_velocity[2] += torque_z * dt;
+
+        // Update attitude
+        spacecraft.updateAttitude();
+        spacecraft.propagateAttitude(dt);
+
+        // Simulate simple circular orbit
+        const orbit_radius = 7000.0;
+        // const x = orbit_radius * @cos(2 * std.math.pi * t / orbital_period);
+        // const y = orbit_radius * @sin(2 * std.math.pi * t / orbital_period);
+        // const z = 0.0;
+
+        try std.testing.expect(orbit_radius > spacecraft.orbiting_object.m_radius.?);
+
+        // try writer.print("{d},{d},{d},{d},{d},{d},{d},{d},{d},{d},{d}\n", .{
+        //     t,
+        //     spacecraft.quaternion[0],
+        //     spacecraft.quaternion[1],
+        //     spacecraft.quaternion[2],
+        //     spacecraft.quaternion[3],
+        //     spacecraft.angular_velocity[0],
+        //     spacecraft.angular_velocity[1],
+        //     spacecraft.angular_velocity[2],
+        //     x,
+        //     y,
+        //     z,
+        // });
+    }
+}
