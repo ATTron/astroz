3784029b22efe15eb4786f82673bb7b96699ad5b
Anthony Templeton <ATTron@users.noreply.github.com>
2026-01-05 00:35:49 -0500

    io changes (#41)

--- examples/parse_vita49.zig
+++ examples/parse_vita49.zig
@@ -7,11 +7,12 @@ pub fn main() !void {
     var dbga = std.heap.DebugAllocator(.{}).init;
     defer _ = dbga.deinit();
     const allocator = dbga.allocator();
+    const io = std.Io.Threaded.global_single_threaded.ioBasic();
 
     const file_name = "./test/vita49.bin".*;
     const sync_pattern = .{ 0x3A, 0x02, 0x0a, 0x00, 0x34, 0x12, 0x00, 0x00, 0x00, 0x56 };
     const P = Parser(Vita49);
-    var parser = try P.init(null, null, 1024, allocator);
+    var parser = try P.init(null, null, 1024, io, allocator);
     defer parser.deinit();
 
     _ = try parser.parseFromFile(&file_name, &sync_pattern, null);
