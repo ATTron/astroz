7fc54ae4d61a7ad6029d0adee6fe95d2f72fded6
Anthony Templeton <twantempleton11@gmail.com>
2026-01-15 15:52:04 -0500

    sgp4 bindings

--- src/c_api/root.zig
+++ src/c_api/root.zig
@@ -4,6 +4,7 @@
 const allocator = @import("allocator.zig");
 pub const err = @import("error.zig");
 const tle = @import("tle.zig");
+const sgp4 = @import("sgp4.zig");
 
 const version = .{ .major = 0, .minor = 3, .patch = 0 };
 
@@ -19,7 +20,6 @@ pub export fn astroz_deinit() callconv(.c) void {
     allocator.deinit();
 }
 
-// TLE
 pub export fn tle_parse(str: [*:0]const u8, out: *tle.Handle) callconv(.c) i32 {
     return @intFromEnum(tle.parse(str, out));
 }
@@ -40,4 +40,14 @@ pub export fn tle_get_eccentricity(h: tle.Handle) callconv(.c) f32 {
 }
 pub export fn tle_get_mean_motion(h: tle.Handle) callconv(.c) f64 {
     return tle.getMeanMotion(h);
+}
+
+pub export fn sgp4_init(tle_h: tle.Handle, grav: i32, out: *sgp4.Handle) callconv(.c) i32 {
+    return @intFromEnum(sgp4.init(tle_h, grav, out));
+}
+pub export fn sgp4_free(handle: sgp4.Handle) callconv(.c) void {
+    sgp4.free(handle);
+}
+pub export fn sgp4_propagate(h: sgp4.Handle, tsince: f64, pos: *[3]f64, vel: *[3]f64) callconv(.c) i32 {
+    return @intFromEnum(sgp4.propagate(h, tsince, pos, vel));
 }
